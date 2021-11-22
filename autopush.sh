e7ba241b1e97571eb671a23bc94babc2af3fe8ea
Hijikesh Hijal <hijikeshhijal@gmail.com>
2021-11-22 21:05:50 +0600

    Update autopush.sh

--- autopush.sh
+++ autopush.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 DATE_N=`/bin/date "+%Y-%m-%d %H:%M:%S"`
-/usr/bin/node linkedin-fulltile-job-scrapper.js
-/usr/bin/node linkedin-intern-job-scrapper.js
+/usr/local/bin/node linkedin-fulltile-job-scrapper.js
 
+/usr/local/bin/git add .
+/usr/local/bin/git commit -m "${DATE_N} Update"
 
-/usr/bin//git add .
-/usr/bin/git commit -m "${DATE_N} Update"
-
-/usr/bin/git push origin master
+/usr/local/bin/git push origin master
 
 echo "Done"
 exit 0
