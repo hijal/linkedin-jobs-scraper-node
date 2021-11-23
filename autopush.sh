bb32d45a2631c12e97f3960ce3e4b47ca1362fb6
imh1j4l <hijal@shuttlebd.com>
2021-11-23 12:03:22 +0600

    update autopush scrip path

--- autopush.sh
+++ autopush.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 DATE_N=`/bin/date "+%Y-%m-%d %H:%M:%S"`
-/usr/local/bin/node linkedin-fulltile-job-scrapper.js
+/usr/bin/node linkedin-fulltile-job-scrapper.js
 
-/usr/local/bin/git add .
-/usr/local/bin/git commit -m "${DATE_N} Update"
+/usr/bin/git add .
+/usr/bin/git commit -m "${DATE_N} Update"
 
-/usr/local/bin/git push origin master
+/usr/bin/git push origin master
 
 echo "Done"
 exit 0
